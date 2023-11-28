51b1c1322ed01e8c3383e0aeb1ec00d153c6893d
Treo <anuar.suleimenov@nu.edu.kz>
2023-11-28 22:02:14 +0600

    edited helper fns

--- helpers.py
+++ helpers.py
@@ -3,6 +3,11 @@ import cv2
 import time
 import requests
 import inspect
+import warnings
+
+from deepface import DeepFace
+
+warnings.filterwarnings("ignore")
 
 def GetResponse(url: str, json : dict, headers: dict) -> bool:
     """
@@ -40,12 +45,34 @@ def logme(sometext: str) -> None:
     print(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())}] - {function_name}: {sometext}\n")
 
 def crop_face(file: str) -> None:
-        img = cv2.imread(file)
-        face_cascade = cv2.CascadeClassifier(f"{cv2.data.haarcascades}haarcascade_frontalface_default.xml")
-        faces = face_cascade.detectMultiScale(img, 1.1, 4)
-        for (x, y, w, h) in faces:
-            face_clip = img[y:y+h, x:x+w]
-            file_name, file_extension = os.path.splitext(file)
-            file = f"{file_name}_cropped{file_extension}"
-            cv2.imwrite(file, face_clip)
-            logme(f"File saved as {file}")
+    backends = [
+        'opencv', 
+        'ssd', 
+        'dlib', 
+        'mtcnn', 
+        'retinaface', 
+        'mediapipe',
+        'yolov8',
+        'yunet',
+        'fastmtcnn',
+    ]
+    # img = cv2.imread(file)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # face_cascade = cv2.CascadeClassifier(f"{cv2.data.haarcascades}haarcascade_frontalface_default.xml")
+    # faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(64, 64), flags = cv2.CASCADE_SCALE_IMAGE)
+    # for (x, y, w, h) in faces:
+    #     face_clip = img[y:y+h, x:x+w]
+    #     file_name, file_extension = os.path.splitext(file)
+    #     file = f"{file_name}_cropped{file_extension}"
+    #     cv2.imwrite(file, face_clip)
+    #     logme(f"File saved as {file}")
+    face_objs = DeepFace.extract_faces(
+        img_path = file, 
+        target_size = (224, 224), 
+        detector_backend = backends[4]
+    )
+    for face_obj in face_objs:
+        print(face_obj)
+        face = face_obj["face"]
+        
+        
